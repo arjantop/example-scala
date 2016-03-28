b6f6feb5e3e16b829ccb28ad36819342ba381956
Arjan Topolovec <arjan@celtra.com>
2016-03-28 22:33:18 +0200

    asd.

--- src/test/scala/com/awesomeness/TestAwesome.scala
+++ src/test/scala/com/awesomeness/TestAwesome.scala
@@ -6,8 +6,8 @@ class TestAwesome extends FunSpec with Matchers {
  val awesome = new Awesomeness
 
  it("sample test") {
-    awesome.square(1) should be (1)
-    // awesome.multiple(2,3) should be (6)
+    // awesome.square(1) should be (1)
+    awesome.multiple(2,3) should be (6)
     Thread.sleep(1000)
   }
 }
